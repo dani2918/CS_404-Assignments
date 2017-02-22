939dec2bd6a13d8267b49f99542f34c469f8990f
dani2918 <dani2918@vandals.uidaho.edu>
2017-02-21 17:37:11 -0800

    working nn

--- HW02/nn/test.cpp
+++ HW02/nn/test.cpp
@@ -57,12 +57,81 @@ void Test::runTest(Matrix &testDat, Matrix &testDatWBias)
 	printf("BEGIN TESTING\n");
 	inputs.setName("x");
 	inputs.print("input normalized");
-	testDat.setName("out");
-	testDat.print();
-	testOutput.writeFormatted();
+	targets.setName("out");
+	targets.print();
+
+	int exp[targets.maxRows()];
+	int max[targets.maxRows()];
+	double maxTargetSoFar = 0;
+	int maxTargetIndexSoFar = 0;
+	double maxTestSoFar = 0;
+	int maxTestIndexSoFar = 0;
+
+	for(int r = 0; r < targets.maxRows(); r++)
+	{
+		for(int c = 0; c < targets.maxCols(); c++)
+		{
+			// printf("%f ", activations.get(r,c));
+			if(activations.get(r,c) > maxTestSoFar)
+			{
+				maxTestSoFar = activations.get(r,c);
+				maxTestIndexSoFar = c;
+			}
+			if(targets.get(r,c) > maxTargetSoFar)
+			{
+				maxTargetSoFar = targets.get(r,c);
+				maxTargetIndexSoFar = c;
+				
+				// printf("exp[r]: %d\n", exp[r]);
+			}
+			
+			// printf("%f\n", max[r]);
+		}
+		exp[r] = maxTargetIndexSoFar;
+		max[r] = maxTestIndexSoFar;
+
+			maxTargetSoFar = 0;
+			maxTargetIndexSoFar = 0;
+			maxTestSoFar = 0;
+			maxTestIndexSoFar = 0;
+		// printf("\n");
+	}
+
+
+	//Set up confusion matrix
+	Matrix confusion = new Matrix(targets.maxCols(), targets.maxCols(), "Confusion");
+	confusion.setName("Confusion");
+	confusion.constant(0.0);
+	for(int row = 0; row < targets.maxRows(); row++)
+	{
+		int r = exp[row]; int c = max[row];
+		// printf("row: %d, col: %d", r, c);
+		int val = (int)confusion.get(r,c);
+		// printf(" val: %d\n", val);
+		val += 1 ;
+		confusion.set(r,c, val);
+	}
+
+
+	// testOutput.writeFormatted();
+	for (int r=0; r<testOutput.maxRows(); r++) 
+	{
+        for (int c=0; c<testOutput.maxCols(); c++) 
+        {
+            printf("%.2f ", testOutput.get(r,c));
+        }
+        printf("EXP: %d MAX: %d",exp[r], max[r]);
+        printf("\n");
+    }
 
 	double dist = sqrt(activations.dist2(targets));
+	
+
+	// Print confusion matrix
+	confusion.print();
 	printf("DIST: %f\n", dist);
+
+
 }
 
 
